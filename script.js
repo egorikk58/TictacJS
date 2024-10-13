e7e6f267f3cfd5f6d65d552c6290dd402d19466e
egorikk58 <egorka.andreev765@gmail.com>
2024-10-13 19:02:55 +0300

    Сделал проверку на винера и проверку на ничью

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let step = "";
 let counter = 0;
 let ElementCell = document.querySelectorAll(".tictac-block-cell");
 let Playa = document.getElementById("Player");
-
+let Winner = document.getElementById("winnerOfGame");
 
 function Who(){
     if (step == "Circle"){
         step = "Cross";
-        Playa.innerHTML = "Крестик";
+        Playa.innerHTML = "Нолик";
     }else{
         step = "Circle";
-        Playa.innerHTML = "Нолик";
+        Playa.innerHTML = "Крестик";
     }
 }
 
@@ -28,7 +28,43 @@ ElementCell.forEach((item) =>{
                 item.innerText = "O";
             }
         }
+        item.style.pointerEvents = 'none';
+
+        counter++;
+        if (counter >= 5 && counter != 9 && isVictory()){
+            if (step == "Circle"){
+                Winner.innerHTML = " Крестик";
+            }else{
+                Winner.innerHTML = " Нолик";
+            }
+            ElementCell.forEach(cell => cell.style.pointerEvents = 'none');
+
+        }
+        else if (counter == 9){
+            Winner.innerText = "Ничья";
+        }
         Who();
     })
     
 })
+
+function isVictory(){
+    let win = [
+        [0,1,2],
+        [0,4,8],
+        [2,4,6],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ]
+    for (let combo of win){
+        if ((ElementCell[combo[0]].classList.contains(step) &&
+        ElementCell[combo[1]].classList.contains(step) &&
+        ElementCell[combo[2]].classList.contains(step))){
+            return true;
+        }    
+    }
+    return false;
+}
